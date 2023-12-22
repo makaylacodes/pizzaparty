440ea2c19202b79619aa756dd9d04f72d4246956
Makayla Anderson-Tucker <makayla.anderson.tucker@gmail.com>
2023-12-21 18:46:13 -0800

    Attached functions to calculate total number of pizzas when the button is clicked.

--- app/src/main/java/com/snhu/pizzaparty/MainActivity.java
+++ app/src/main/java/com/snhu/pizzaparty/MainActivity.java
@@ -1,14 +1,46 @@
 package com.snhu.pizzaparty;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    public final int SLICES_PER_PIZZA = 8;
+
+    private EditText mNumAttendEditText;
+    private TextView mNumPizzasTextView;
+    private RadioGroup mHowHungryRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNumAttendEditText = findViewById(R.id.num_attend_edit_text);
+        mNumPizzasTextView = findViewById(R.id.num_pizzas_text_view);
+        mHowHungryRadioGroup = findViewById(R.id.hungry_radio_group);
+    }
+
+    public void calculateClick(View view) {
+        String numAttendStr = mNumAttendEditText.getText().toString();
+        int numAttend = Integer.parseInt(numAttendStr);
+
+        // Determine how many slices on average each person will eat
+        int slicesPerPerson = 0;
+        int checkedId = mHowHungryRadioGroup.getCheckedRadioButtonId();
+        if (checkedId == R.id.light_radio_button) {
+            slicesPerPerson = 2;
+        }
+        else if (checkedId == R.id.medium_radio_button) {
+            slicesPerPerson = 3;
+        }
+        else if (checkedId == R.id.ravenous_radio_button) {
+            slicesPerPerson = 4;
+        }
+        int totalPizzas = (int) Math.ceil(numAttend * slicesPerPerson /
+                (double) SLICES_PER_PIZZA);
+        mNumPizzasTextView.setText("Total pizzas: " + totalPizzas);
     }
 }
